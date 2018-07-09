d4d543c3d79c7fd0eedaef839f0f62fcb880f34d
vbusovikov <vbusovikov@diasoft.ru>
2018-07-09 17:50:31 +0500

    initial commit

---
+++ app/src/main/java/ru/markstudio/kafkaiot/view/MainActivity.kt
@@ -0,0 +1,27 @@
+package ru.markstudio.kafkaiot.view
+
+import android.net.Uri
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.markstudio.kafkaiot.R
+import ru.markstudio.kafkaiot.view.fragments.StartFragment
+
+class MainActivity : AppCompatActivity(), StartFragment.OnFragmentInteractionListener {
+
+    override fun onFragmentInteraction(uri: Uri) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        navigateToStart()
+    }
+
+    fun navigateToStart() {
+        supportFragmentManager.beginTransaction().add(
+                R.id.content,
+                StartFragment()
+        ).commitAllowingStateLoss()
+    }
+}
