776dd8ad5ba7a40d9b292f9dcda3e1d8d6c36623
vbusovikov <vbusovikov@diasoft.ru>
2018-07-12 09:14:33 +0500

    Начал добавлять функционал по считыванию NFC меток

--- app/src/main/java/ru/markstudio/kafkaiot/view/MainActivity.kt
+++ app/src/main/java/ru/markstudio/kafkaiot/view/MainActivity.kt
@@ -7,23 +7,11 @@ import androidx.navigation.findNavController
 import ru.markstudio.kafkaiot.R
 import ru.markstudio.kafkaiot.view.fragments.StartFragment
 
-class MainActivity : AppCompatActivity(), StartFragment.OnFragmentInteractionListener {
-
-    override fun onFragmentInteraction(uri: Uri) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        navigateToStart()
-    }
-
-    fun navigateToStart() {
-        supportFragmentManager.beginTransaction().add(
-                R.id.content,
-                StartFragment()
-        ).commitAllowingStateLoss()
     }
 
     override fun onSupportNavigateUp() = findNavController(R.id.nav_host_fragment).navigateUp()
